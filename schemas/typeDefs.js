6afe035665ef08dff4210ed31262723ea0311eda
adam-algatt <adam.algatt@gmail.com>
2022-06-12 21:42:48 -0700

    stopping point 12 June

---
+++ schemas/typeDefs.js
@@ -0,0 +1,19 @@
+// import gql tagged template function
+const { gql } = require('apollo-server-express');
+
+//create typeDefs
+const typeDefs = gql`
+type Thought {
+    _id: ID 
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+}
+type Query {
+   thoughts(username: String): [Thought]
+}
+`;
+
+//export typeDefs
+module.exports = typeDefs;
